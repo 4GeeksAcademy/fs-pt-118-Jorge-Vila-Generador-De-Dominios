fb3597b78963d0f794e75ca348d9303359ab8b06
Jorge Vila <45121848+jorviltim@users.noreply.github.com>
2025-08-04 16:25:27 +0000

    feat: Finish domain generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  // Here I create the name, adjective, and noun for the domain we want to create
+  let pronoun = ['the', 'our', "your"];
+  let adj = ['great', 'big',"small"];
+  let noun = ['jogger', 'racoon',"eggle"];
+  //// Here I create an array to, you know, push all the domain names into it.
+  let domain = [];
+
+  // This is the unbelievable for loop where the magic happens and combines all the
+  //  pronouns, adjectives, and nouns. Cool, right?
+  for (let i of pronoun) {
+    for (let j of adj) {
+      for (let k of noun) {
+        //here introduce one by one to the domain array each domain created,
+        //  example: https//www.thegreatjogger.com
+        domain.push(`https//www.${i}${j}${k}.com`);
+      }
+    }
+  }
+
+  //when all the domain names are saved into the array , we can can do a foreach loop
+  //to show all the domain names saved 
+  domain.forEach((indice) => {
+    console.log(indice);
+  });
+
 };
