c2e67151f86f4332058c70181a81d7ee5b12a855
Jorge Vila <45121848+jorviltim@users.noreply.github.com>
2025-08-04 16:50:01 +0000

    feat: now we can check the domain list also from the web

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ import "./assets/img/4geeks.ico";
 window.onload = function () {
   // Here I create the name, adjective, and noun for the domain we want to create
   let pronoun = ['the', 'our', "your"];
-  let adj = ['great', 'big',"small"];
-  let noun = ['jogger', 'racoon',"eggle"];
+  let adj = ['great', 'big', "small"];
+  let noun = ['jogger', 'racoon', "eggle"];
   //// Here I create an array to, you know, push all the domain names into it.
   let domain = [];
 
@@ -24,11 +24,19 @@ window.onload = function () {
       }
     }
   }
-
   //when all the domain names are saved into the array , we can can do a foreach loop
   //to show all the domain names saved 
-  domain.forEach((indice) => {
-    console.log(indice);
+  domain.forEach((domain) => {
+   //to see all the domain names from the console
+    console.log(domain);
+   
+   //to see all the domain names from the web page using an ul list
+    const ulist = document.getElementById("ul");
+    const list = document.createElement("li");
+    list.innerText = domain;
+    ulist.appendChild(list);
   });
+
+
 
 };
